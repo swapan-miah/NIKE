ce52b228f4f043d3536813a1c8b6475ab877a751
swapan-miah <swaponmiah120@gmail.com>
2023-05-18 22:46:22 +0600

    verification section is complite

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var ResetPasswrodAlertBox = document.querySelector(".ResetPasswrodAlertBox");
+var btnResetPasswordForgot = document
+  .querySelector(".btn-ResetPasswordForgot")
+  .addEventListener("click", function () {
+    ResetPasswrodAlertBox.classList.add("visibleForRestBtnClick");
+  });
