85af22f719a347a4256576c53f9b6257507dd4ea
swapan-miah <swaponmiah120@gmail.com>
2023-05-20 23:37:30 +0600

    home section html code

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ var btnResetPasswordForgot = document
   .addEventListener("click", function () {
     ResetPasswrodAlertBox.classList.add("visibleForRestBtnClick");
   });
+
+// ........ owl carousel start ........
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+// .........owl carousel end  .........
